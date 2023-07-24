1f097b235d2e75fb0124b792e1096ac3ceefc127
Elad Abutbul <eladabutbul61@gmail.com>
2023-07-24 22:35:55 +0300

    I built more components to split the code, you made in this commit, a component that contains the content of the questions

---
+++ src/components/Trivia/Questions/body/index.jsx
@@ -0,0 +1,23 @@
+import React, { useContext } from "react";
+import { View } from "react-native";
+import { triviaContext } from "../../../../contextApi/triviaContext";
+import { TriviaQuestions } from "../../../../TriviaQuestions";
+import { Slider } from "../../../Generic";
+import { TriviaListItem } from "../../triviaListItem";
+import { triviaStyle } from "../../../../screens/Trivia/triviaStyle";
+
+export const Body = () => {
+  const { handleSave, activeSlider, handlePressItem } = useContext(triviaContext);
+  return (
+    <View style={triviaStyle.body}>
+      <Slider
+        handleSave={handleSave}
+        sliders={TriviaQuestions}
+        activeSlider={activeSlider}
+        handlePressItem={handlePressItem}
+        renderItem={TriviaListItem}
+        styleRenderItem={[triviaStyle.renderItem, triviaStyle.button]}
+      />
+    </View>
+  );
+};
